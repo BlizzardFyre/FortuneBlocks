2e2398a709e489a6d02ee7d4a2f09f461bbb7281
Michael Ziluck <plugindev68@gmail.com>
2014-10-30 21:34:29 -0400

    Add Features #1
    - Added ability to add and remove items from item in hand.

--- src/com/blizzardfyre/fortuneblocks/CommandHandler.java
+++ src/com/blizzardfyre/fortuneblocks/CommandHandler.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandHandler implements CommandExecutor {
 
@@ -15,13 +16,25 @@ public class CommandHandler implements CommandExecutor {
 			return true;
 		}
 
-		if (args.length != 2) {
+		if (args.length < 1 || args.length > 2) {
 			help(sender);
 			return true;
 		}
 
 		if (args[0].equalsIgnoreCase("add")) {
-			Material mat = Material.getMaterial(args[1].toUpperCase());
+			Material mat;
+			if (args.length == 1) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(ChatColor.RED + "Non-players have to specify the item they want to add.");
+					return true;
+				}
+				Player p = (Player) sender;
+				if (p.getItemInHand().getType() == Material.AIR) {
+					sender.sendMessage(ChatColor.RED + "You can't add air to the item list.");
+					return true;
+				}
+				mat = p.getItemInHand().getType();
+			} else mat = Material.getMaterial(args[1].toUpperCase());
 			if (mat == null) {
 				sender.sendMessage(Main.getPrefix() + ChatColor.RED + "That is not a valid material.");
 				return true;
@@ -29,7 +42,19 @@ public class CommandHandler implements CommandExecutor {
 			Main.getMain().addMaterial(mat);
 			sender.sendMessage(Main.getPrefix() + ChatColor.GOLD + "Material added.");
 		} else if (args[0].equalsIgnoreCase("remove")) {
-			Material mat = Material.getMaterial(args[1].toUpperCase());
+			Material mat;
+			if (args.length == 1) {
+				if (!(sender instanceof Player)) {
+					sender.sendMessage(ChatColor.RED + "Non-players have to specify the item they want to remove.");
+					return true;
+				}
+				Player p = (Player) sender;
+				if (p.getItemInHand().getType() == Material.AIR) {
+					sender.sendMessage(ChatColor.RED + "You can't remove nothing from the item list.");
+					return true;
+				}
+				mat = p.getItemInHand().getType();
+			} else mat = Material.getMaterial(args[1].toUpperCase());
 			if (mat == null) {
 				sender.sendMessage(Main.getPrefix() + ChatColor.RED + "That is not a valid material.");
 				return true;
